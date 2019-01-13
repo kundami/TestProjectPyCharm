d4f00b043187c439d8ba0f63b374f2f333392762
Amitabha Kundu <amitabhak02@gmail.com>
2019-01-12 20:25:04 -0500

    Test Commit from Pycharm

--- test_is_prime.py
+++ test_is_prime.py
@@ -9,3 +9,7 @@ class TestIs_prime(TestCase):
     def test_is_not_prime(self):
         from primes import is_prime
         self.assertFalse(is_prime(6))
+
+    def test_is_not_prime_fail(self):
+        from primes import is_prime
+        self.assertFalse(is_prime(5))
